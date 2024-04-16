4f41ad2b46e3037f85c3ef49b1a5733007590e61
Kielx <kiel66@gmail.com>
2024-04-16 23:55:11 +0200

    Remove createPdfButton from main page

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -15,12 +15,6 @@ const Dashboard = () => {
   return (
     <div className="w-full h-full p-4 sm:p-10 m-auto space-y-4 lg:grid lg:grid-cols-2 lg:space-y-0 lg:gap-4 xl:grid-cols-3 max-w-screen-2xl">
       Dashboard
-      <CreatePdfButton
-        city="Kielce"
-        theme="Historyczne"
-        participants={16}
-        description={loremIpsum}
-      />
     </div>
   );
 };
