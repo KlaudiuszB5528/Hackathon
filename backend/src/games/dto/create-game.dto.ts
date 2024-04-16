b2d2632c2f8aba6f526e26ef9891a3cf57f6343a
MarekDzwonnik <mardzw7@gmail.com>
2024-04-16 21:30:56 +0200

    added game entity and all operations

--- backend/src/games/dto/create-game.dto.ts
+++ backend/src/games/dto/create-game.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, Max } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateGameDto {
@@ -7,6 +7,7 @@ export class CreateGameDto {
   @ApiProperty()
   title: string;
   @IsNumber()
+  @Max(16)
   @IsNotEmpty()
   @ApiProperty()
   slots: number;
