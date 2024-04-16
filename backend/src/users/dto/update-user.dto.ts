21eac52a18da23d4ccc1bc9ce73d648d52966b02
MarekDzwonnik <mardzw7@gmail.com>
2024-04-17 00:08:10 +0200

    fixed documentation

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,10 +1,4 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import { IsEnum } from 'class-validator';
-import { Role } from '../enums/roles';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsEnum(Role)
-  @ApiProperty()
-  role: Role;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
