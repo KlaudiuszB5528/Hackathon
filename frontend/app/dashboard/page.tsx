f59995baf131c907a886b9b8c70b86ace98cff2a
Kielx <kiel66@gmail.com>
2024-04-17 04:49:53 +0200

    Add master user dashboard

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,18 +1,63 @@
 'use client';
-
-import { redirect } from 'next/navigation';
-import { useContext } from 'react';
-import { IAuthContext } from '../Interfaces/IAuthContext';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { IAuthContext } from '../Interfaces/IAuthContext';
+import { getCookie, deleteCookie } from 'cookies-next';
+import GameCard from './GameCard';
+import { IGameDetails } from '../Interfaces/IGameDetails';
 
-const Dashboard = () => {
-  const { userData } = useContext(AuthContext) as IAuthContext;
-  if (userData?.role === 'admin') redirect('/dashboard/admin');
+const User = () => {
+  const { userId, loading, setLoading } = useContext(
+    AuthContext,
+  ) as IAuthContext;
+  const [games, setGames] = useState<any>([]);
+  console.log(games);
+  useEffect(() => {
+    const fetchData = async () => {
+      // Fetch data from the backend
+      try {
+        const response = await fetch(
+          `http://localhost:8000/games/game-master/${userId}`,
+          {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${getCookie('token')}`,
+            },
+          },
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setGames(data);
+        }
+      } catch (error) {
+        // If response is not ok then redirect to sign in page
+        deleteCookie('token');
+        window.location.href = '/signin';
+      }
+    };
+    setLoading(true);
+    fetchData();
+    setLoading(false);
+  }, [userId, setLoading, setGames]);
+
   return (
     <div className="w-full h-full p-4 sm:p-10 m-auto space-y-4 lg:grid lg:grid-cols-2 lg:space-y-0 lg:gap-4 xl:grid-cols-3 max-w-screen-2xl">
-      Dashboard
+      {games &&
+        games.map((game: IGameDetails) => (
+          <GameCard
+            key={game.gameId}
+            gameId={game.gameId}
+            author={game.author}
+            city={game.city}
+            gameRules={game.gameRules}
+            participants={game.participants}
+            promptResponse={game.promptResponse}
+            theme={game.theme}
+            title={game.title}
+          />
+        ))}
     </div>
   );
 };
 
-export default Dashboard;
+export default User;
