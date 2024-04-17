3767f1e0659b5744e3506e212b4129d3012b2116
Kielx <kiel66@gmail.com>
2024-04-17 03:19:26 +0200

    Add game cards

---
+++ frontend/app/user/GameCard.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import { IAuthContext } from '@/app/Interfaces/IAuthContext';
+import { AuthContext } from '@/app/context/AuthContext';
+import { useContext } from 'react';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import { Button } from '@/components/ui/button';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Skeleton } from '@/components/ui/skeleton';
+import { BarChartBig, FileDigit, Star, WholeWord } from 'lucide-react';
+import Link from 'next/link';
+
+const GameCard = ({
+  gameId,
+  author,
+  city,
+  gameRules,
+  participants,
+  promptResponse,
+  theme,
+  title,
+}) => {
+  const { loading } = useContext(AuthContext) as IAuthContext;
+  // If loading then show loading spinner
+  if (loading) {
+    return (
+      <div className="flex flex-col space-y-6 w-full p-6 border border-gray-200 rounded-lg">
+        <Skeleton className="h-8 w-1/2 rounded-xl " />
+        <Skeleton className="h-4 w-[250px] rounded-xl" />
+
+        <div className="flex gap-2 items-center">
+          <Skeleton className="h-6 w-6 rounded-full" />
+          <Skeleton className="h-6 w-[175px] rounded-xl" />
+        </div>
+        <div className="flex gap-2 items-center">
+          <Skeleton className="h-6 w-6 rounded-full" />
+          <Skeleton className="h-6 w-[175px] rounded-xl" />
+        </div>
+        <div className="flex gap-2 items-center">
+          <Skeleton className="h-6 w-6 rounded-full" />
+          <Skeleton className="h-6 w-[175px] rounded-xl" />
+        </div>
+        <Skeleton className="mt-4 h-10 w-[200px] rounded-xl" />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Card className="flex flex-col">
+        <CardHeader className="border-b border-gray-200 mb-2">
+          <CardTitle className="flex gap-2 items-center">
+            <BarChartBig className="h-8 w-8" />
+            {title}
+          </CardTitle>
+          <CardDescription>{city}</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-1">
+          <span className="sr-only">Team Information</span>
+          <div className="flex items-center gap-2.5 ">
+            <FileDigit className="h-5 w-5" />
+            <p className="font-bold">Game Number </p>
+          </div>
+          <p className="pb-3">{gameId}</p>
+          <div className="flex items-center gap-2.5">
+            <Star className="h-5 w-5" />
+            <p className="font-bold">Game Theme</p>
+          </div>
+          <p className="pb-3">{theme}</p>
+          <div className="flex items-center gap-2.5 pb-1">
+            <WholeWord className="h-5 w-5" />
+            <p className="font-bold">Game Rules</p>
+          </div>
+          <ScrollArea className="pt-2 h-[100px] w-full rounded-md border-l px-2">
+            {gameRules}
+          </ScrollArea>
+        </CardContent>
+        <CardFooter className="mt-auto">
+          <Button asChild className="bg-fuchsia-700">
+            <Link href="/dashboard/settings">Update Team Information</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+};
+
+export default GameCard;
--- frontend/app/user/page.tsx
+++ frontend/app/user/page.tsx
@@ -1,7 +1,57 @@
+'use client';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { IAuthContext } from '../Interfaces/IAuthContext';
+import { getCookie, deleteCookie } from 'cookies-next';
+import GameCard from './GameCard';
+
 const User = () => {
+  const { userId, loading, setLoading } = useContext(
+    AuthContext,
+  ) as IAuthContext;
+  const [games, setGames] = useState<any>([]);
+  console.log(games);
+  useEffect(() => {
+    const fetchData = async () => {
+      // Fetch data from the backend
+      try {
+        const response = await fetch(`http://localhost:8000/games`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${getCookie('token')}`,
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setGames(data);
+        }
+      } catch (error) {
+        // If response is not ok then redirect to sign in page
+        deleteCookie('token');
+        window.location.href = '/signin';
+      }
+    };
+    setLoading(true);
+    fetchData();
+    setLoading(false);
+  }, [userId, setLoading, setGames]);
+
   return (
     <div className="w-full h-full p-4 sm:p-10 m-auto space-y-4 lg:grid lg:grid-cols-2 lg:space-y-0 lg:gap-4 xl:grid-cols-3 max-w-screen-2xl">
-      User
+      {games &&
+        games.map((game) => (
+          <GameCard
+            key={game.id}
+            gameId={game.id}
+            author={game.author}
+            city={game.city}
+            gameRules={game.gameRules}
+            participants={game.participants}
+            promptResponse={game.promptResponse}
+            theme={game.theme}
+            title={game.title}
+          />
+        ))}
     </div>
   );
 };
